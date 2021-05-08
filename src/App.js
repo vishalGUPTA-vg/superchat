6b7f41af30d3ee52656d81a0857bc6b32993eefb
Devang Mathur <masterdevang@gmail.com>
2021-05-08 16:45:30 +0530

    alt prop

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ function ChatMessage(props) {
 
   return (<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <img alt="" src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
       <p>{text}</p>
     </div>
   </>)
